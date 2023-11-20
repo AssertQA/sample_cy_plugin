440ea792eff81931921f64aecc5c5a464f1c138d
Marko Kolasinac <marko.kolasinac@assertqa.com>
2023-11-20 17:09:32 +0100

    for david

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
-// index.js
-const { existsSync, mkdirSync, writeFileSync } = require('fs');
-const { join } = require('path');
 
-function createFile() {
-  const folderName = 'TargetFolder';
-  const fileName = 'targetfile.json';
-  const filePath = join(folderName, fileName);
-  const unixTimestamp = Math.floor(Date.now() / 1000);
-  const data = { timestamp: unixTimestamp };
-  const jsonData = JSON.stringify(data, null, 2);
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
+import { join } from 'path';
 
-  if (!existsSync(folderName)) {
+//const projectPath = './'; // *** Replace with  Cypress Path ***
+const folderName = 'TargetFolder'; 
+const fileName = 'targetfile.json'; 
+
+
+// Construct the full path to the file within the folder
+const filePath = join(folderName, fileName);
+
+
+// Get the current Unix timestamp
+const unixTimestamp = Math.floor(Date.now() / 1000);
+
+// Create an object with the timestamp
+const data = {
+  timestamp: unixTimestamp,
+};
+
+// Convert the object to JSON
+const jsonData = JSON.stringify(data, null, 2); // The third parameter is for indentation (2 spaces in this case)
+
+// Check if the folder exists
+if (!existsSync(folderName)) {
+    // If the folder doesn't exist, create it
     mkdirSync(folderName, { recursive: true });
     console.log(`Folder '${folderName}' created.`);
-  }
+} 
 
-  if (!existsSync(filePath)) {
+
+// Check if the file exists
+if (!existsSync(filePath)) {
+    // If the file doesn't exist, create an sample  file
     writeFileSync(filePath, jsonData);
     console.log(`File '${fileName}' created in '${folderName}'.`);
-  } else {
+} else {
     writeFileSync(filePath, jsonData);
     console.log(`File '${fileName}' already exists in '${folderName}'. Regenerating....`);
-  }
 }
-
-//module.exports = createFile;
---
+++ src/oldIndex.js
@@ -0,0 +1,28 @@
+// index.js
+const { existsSync, mkdirSync, writeFileSync } = require('fs');
+const { join } = require('path');
+
+function createFile() {
+  const folderName = 'TargetFolder';
+  const fileName = 'targetfile.json';
+  const filePath = join(folderName, fileName);
+  const unixTimestamp = Math.floor(Date.now() / 1000);
+  const data = { timestamp: unixTimestamp };
+  const jsonData = JSON.stringify(data, null, 2);
+
+  if (!existsSync(folderName)) {
+    mkdirSync(folderName, { recursive: true });
+    console.log(`Folder '${folderName}' created.`);
+  }
+
+  if (!existsSync(filePath)) {
+    writeFileSync(filePath, jsonData);
+    console.log(`File '${fileName}' created in '${folderName}'.`);
+  } else {
+    writeFileSync(filePath, jsonData);
+    console.log(`File '${fileName}' already exists in '${folderName}'. Regenerating....`);
+  }
+}
+
+
+//module.exports = createFile;
